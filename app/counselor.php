7198748a281bb18e87e137fd0b7f927a50fb16c5
HeribertoBourdon <redx117>
2018-01-04 10:58:42 -0400

    Added changes to counselor

--- app/counselor.php
+++ app/counselor.php
@@ -8,7 +8,7 @@ class counselor extends Model
 {
     protected $hidden = ['created_at','updated_at','pivot'];
     //
-    protected $fillable = ['fullName','counselorEmail','counselorPhone','counselorFaculty','counselorDepartment','counselorOffice'];
+    protected $fillable = ['counselorName','counselorEmail','counselorPhone','counselorFaculty','counselorDepartment','counselorOffice'];
 
     public function organizations(){
         return $this->belongsToMany('App\organization','counsels',
