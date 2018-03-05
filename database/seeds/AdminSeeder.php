7c8984a98f29c8dffefbb38e3ca7d7eef48d5f9f
Jesse Villafane <jesse.villafane@upr.edu>
2018-03-05 18:46:18 -0400

    Add jesse.villafane@upr.edu

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -14,18 +14,18 @@ class AdminSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            ['id'=>1,'userEmail'=>'jesmarie.hernandez@upr.edu','userType_code'=>1]
+            ['id'=>1,'userEmail'=>'jesse.villafane@upr.edu','userType_code'=>1]
         ]);
 
         DB::table('staff')->insert([
             ['id'=>1,'staffName'=>'Jesmarie Hernandez',
-                'staffEmail'=>'jesmarie.hernandez@upr.edu','staffPhone'=>'7871234567',
+                'staffEmail'=>'jesse.villafane@upr.edu','staffPhone'=>'7871234567',
                 'staffType_code'=>1,'user_id'=>1,'isActive'=>1]
         ]);
 
         DB::table('facilities_managers')->insert([
             ['id'=>1,'managerName'=>'Jesmarie Hernandez',
-                'managerEmail'=>'jesmarie.hernandez@upr.edu','managerPhone'=>'7871234567',
+                'managerEmail'=>'jesse.villafane@upr.edu','managerPhone'=>'7871234567',
                 'user_id'=>1,'isActive'=>0]
         ]);
 
