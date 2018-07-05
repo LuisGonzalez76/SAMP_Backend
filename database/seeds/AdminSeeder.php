f16eef98b06aaaebd8c8c1a831fd6d24906a7da0
jesse-villafane <jesse.villafane@upr.edu>
2018-07-05 16:55:07 -0400

    Adds more users to the admin seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -14,19 +14,26 @@ class AdminSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            ['id'=>1,'userEmail'=>'jesse.villafane@upr.edu','userType_code'=>1]
+            ['id'=>1,'userEmail'=>'jesse.villafane@upr.edu','userType_code'=>1],
+            ['id'=>2,'userEmail'=>'yomarachaliff.lucianofigueroa@upr.edu','userType_code'=>1]
         ]);
 
         DB::table('staff')->insert([
-            ['id'=>1,'staffName'=>'Jesmarie Hernandez',
-                'staffEmail'=>'jesse.villafane@upr.edu','staffPhone'=>'7871234567',
-                'staffType_code'=>1,'user_id'=>1,'isActive'=>1]
+            ['id'=>1,'staffName'=>'Jesse Villafane',
+                'staffEmail'=>'jesse.villafane@upr.edu','staffPhone'=>'9090',
+                'staffType_code'=>1,'user_id'=>1,'isActive'=>1],
+            ['id'=>1,'staffName'=>'Yomarachaliff Luciano',
+                'staffEmail'=>'yomarachaliff.lucianofigueroa@upr.edu','staffPhone'=>'9090',
+                'staffType_code'=>1,'user_id'=>2,'isActive'=>1]
         ]);
 
         DB::table('facilities_managers')->insert([
-            ['id'=>1,'managerName'=>'Jesmarie Hernandez',
-                'managerEmail'=>'jesse.villafane@upr.edu','managerPhone'=>'7871234567',
-                'user_id'=>1,'isActive'=>0]
+            ['id'=>1,'managerName'=>'Jesse Villafane',
+                'managerEmail'=>'jesse.villafane@upr.edu','managerPhone'=>'9090',
+                'user_id'=>1,'isActive'=>0],
+            ['id'=>2,'managerName'=>'Yomarachaliff Luciano',
+                'managerEmail'=>'yomarachaliff.lucianofigueroa@upr.edu','managerPhone'=>'9090',
+                'user_id'=>2,'isActive'=>0]
         ]);
 
 
--- database/seeds/facilitiesSeeder.php
+++ database/seeds/facilitiesSeeder.php
@@ -13,18 +13,18 @@ class facilitiesSeeder extends Seeder
     {
         //
         DB::table('facilities')->insert([
-            ['id'=>1,'building'=>'Chardon','space'=>'Figueroa Chapel',
-                'facilityDepartment'=>'Actividades Sociales','isActive'=>1],
-            ['id'=>2,'building'=>'Centro de Estudiantes','space'=>'Salón Tarzan',
-                'facilityDepartment'=>'Actividades Sociales','isActive'=>1],
-            ['id'=>3,'building'=>'Chardon','space'=>'La Placita',
-                'facilityDepartment'=>'Actividades Sociales','isActive'=>1],
-            ['id'=>4,'building'=>'Centro de Estudiantes','space'=>'Tercer Piso',
-                'facilityDepartment'=>'Actividades Sociales','isActive'=>1],
-            ['id'=>5,'building'=>'Stefani','space'=>'S-230',
-                'facilityDepartment'=>'Ingeniería Mecánica','isActive'=>1],
-            ['id'=>6,'building'=>'Stefani','space'=>'Lobby',
-                'facilityDepartment'=>'Ingeniería Eléctrica','isActive'=>1],
+            ['id' => 1, 'building' => 'Chardon', 'space' => 'Figueroa Chapel',
+                'facilityDepartment' => 'Actividades Sociales', 'isActive' => 1],
+            ['id' => 2, 'building' => 'Centro de Estudiantes', 'space' => 'Salón Tarzan',
+                'facilityDepartment' => 'Actividades Sociales', 'isActive' => 1],
+            ['id' => 3, 'building' => 'Chardon', 'space' => 'La Placita',
+                'facilityDepartment' => 'Actividades Sociales', 'isActive' => 1],
+            ['id' => 4, 'building' => 'Centro de Estudiantes', 'space' => 'Tercer Piso',
+                'facilityDepartment' => 'Actividades Sociales', 'isActive' => 1],
+            ['id' => 5, 'building' => 'Stefani', 'space' => 'S-230',
+                'facilityDepartment' => 'Ingeniería Mecánica', 'isActive' => 1],
+            ['id' => 6, 'building' => 'Stefani', 'space' => 'Lobby',
+                'facilityDepartment' => 'Ingeniería Eléctrica', 'isActive' => 1],
         ]);
     }
 }
